7cd6a88d610b5193fd61439b2dfc1262ba2f2717
Dev-Santo44 <74419028+Dev-Santo44@users.noreply.github.com>
2025-05-31 19:54:16 +0530

    Update firebase.js

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -2,13 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyC7zN8d8YHJNxPx93bi7v_YQp_4IJztFfA",
-    authDomain: "kraftspark-cb0d8.firebaseapp.com",
-    projectId: "kraftspark-cb0d8",
-    storageBucket: "kraftspark-cb0d8.firebasestorage.app",
-    messagingSenderId: "291047627099",
-    appId: "1:291047627099:web:bec3713ec5409aa279227e",
-    measurementId: "G-CFDTPPF9Y8"
+    //here your firebase configuration
+    
 };
 
 // Initialize Firebase
